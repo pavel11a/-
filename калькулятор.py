bdff6c2be98410d8c8195f78730941c45362a137
pavel11a <93409391+pavel11a@users.noreply.github.com>
2021-10-30 01:03:12 +0300

    Add files via upload

---
+++ калькулятор.py
@@ -0,0 +1,18 @@
+
+# калькулятор
+what = input("Действие (+,-,/,*):")
+a = float ( input ("Первое число:") )
+b = float ( input ("Второе число:") )
+
+if what == "+":
+	c = a + b
+	print ("Результат:" + с)
+elif what == "-":
+	c = a - b
+	print ("Результат:" + str(с))
+elif what == "/":
+	c = a / b
+	print ("Результат:" + str(с))
+elif what == "*":
+	c = a * b
+	print ("Результат:" + str(с))
